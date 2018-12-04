4e176b390f26108fd5a9692d793cfe7e1cf7312d
Vladimir Kivernik <vladimir@kivernik.ru>
2018-12-04 11:08:31 +0300

    Добавил JSON валидацию обязательных полей лабораторных данных

--- Models/Leptospirosis/LaboratoryData.cs
+++ Models/Leptospirosis/LaboratoryData.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
 
- namespace medicaldss_api.Models.Leptospirosis
+namespace medicaldss_api.Models.Leptospirosis
  {
      public class LaboratoryData
      {
@@ -16,12 +18,19 @@ using Microsoft.AspNetCore.Mvc;
         public const int fvCoeff = 100;
         public const double rkfmCoeff = 3.4;
 
+        [JsonRequired]
         public double tromb { get; set; }
+        [JsonRequired]
         public double pti { get; set; }
+        [JsonRequired]
         public double fibr { get; set; }
+        [JsonRequired]
         public double achtv { get; set; }
+        [JsonRequired]
         public double sag { get; set; }
+        [JsonRequired]
         public double fv { get; set; }
+        [JsonRequired]
         public double rkfm { get; set; }
 
         public double [] getMarks()
